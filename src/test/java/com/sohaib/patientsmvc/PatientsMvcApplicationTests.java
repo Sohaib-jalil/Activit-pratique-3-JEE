aa55c35c4d54d90c3b2b2a2b31635deb54cad417
Sohaib <jalilsohaib3@gmail.com>
2023-04-14 22:08:37 +0000

    initial commit

---
+++ src/test/java/com/sohaib/patientsmvc/PatientsMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sohaib.patientsmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientsMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
